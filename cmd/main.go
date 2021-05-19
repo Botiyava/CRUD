78a81bc1b5eef3f8e8adc3a1c79411ae04c32fb3
botiyava <botiyava.prog@mail.ru>
2021-05-19 22:22:19 +0300

    Внедрение зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/Botiyava/todo-app"
 	"github.com/Botiyava/todo-app/pkg/handler"
+	"github.com/Botiyava/todo-app/pkg/repository"
+	"github.com/Botiyava/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
