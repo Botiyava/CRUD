67ce2c1ac2785e5ffacc280f8bd8a186782e211b
botiyava <botiyava.prog@mail.ru>
2021-05-19 19:38:51 +0300

    #2

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/Botiyava/todo-app"
+	"github.com/Botiyava/todo-app/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 
